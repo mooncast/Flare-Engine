a89c6fb693f2ab81e98c44747ebb3eb40f0314cb
Casper Fink <cypeyt@gmail.com>
2022-12-18 22:45:39 +0100

    Init git

---
+++ Flare Engine/Flare Engine Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using SDL2;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+try
+{
+    var window = SDL.SDL_CreateWindow("TEST", 0, 0, 600, 600, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+    var Render = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+    SDL.SDL_SetHint(SDL.SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+}
+catch (Exception e)
+{
+    Console.WriteLine(
+    SDL.SDL_GetError());
+}
+
+
+
